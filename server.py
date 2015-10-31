8b27f7852c6f8be3bc497dc9cf0b598e7ac93202
Timo Harmonen <timo.harmonen@iki.fi>
2015-10-31 19:21:24 +0200

    Simple post data handling

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -11,5 +11,21 @@ def hello():
 @app.route('/test', methods=['GET', 'POST'])
 def test():
     print("request.method:{}".format(request.method))
+    name=request.form['name']
+    return "Hello {}\n".format(name)
 
-    return "Hello tester"
+
+@app.route('/v1/post_data', methods=['GET', 'POST'])
+def post_data():
+    print("/v1/post_data")
+    data=request.form['data']
+    start=request.form['start']
+    end=request.form['end']
+    print("start:'{}'".format(start))
+    print("end:'{}'".format(end))
+    print("data:'{}'".format(data))
+    return "Got data:'{}'\n".format(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
